d7e101647261a3d62ac238799b32d33e487cf72a
fjlinww <34429052+fjlinww@users.noreply.github.com>
2020-10-20 10:17:27 +0800

    Update ChangePermissions.sh

--- ChangePermissions.sh
+++ ChangePermissions.sh
@@ -1,3 +1,7 @@
+# 解决报错：Got permission denied while trying to connect to the Docker daemon socket at 
+# unix:///var/run/docker.sock: Get http://%2Fvar%2Frun%2Fdocker.sock/v1.40/containers/json: 
+# dial unix /var/run/docker.sock: connect: permission denied
+
 #添加docker用户组
 sudo groupadd docker
 
