d4b7dede8e08f7aedd41b6e6be466ecf0c634c30
fjlinww <34429052+fjlinww@users.noreply.github.com>
2020-10-20 09:59:13 +0800

    Update INSTALLCOMPOSE.sh

--- INSTALLCOMPOSE.sh
+++ INSTALLCOMPOSE.sh
@@ -1,8 +1,11 @@
 # Run this command to download the current stable release of Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0-rc4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
 # Apply executable permissions to the binary
 sudo chmod +x /usr/local/bin/docker-compose
+
 # create a symbolic link to /usr/bin
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 # Test
 docker-compose --version
