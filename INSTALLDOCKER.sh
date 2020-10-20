3b2def30b23455d27cac2e90223fa02c5d62dd5a
fjlinww <34429052+fjlinww@users.noreply.github.com>
2020-10-20 09:55:26 +0800

    Update INSTALLDOCKER.sh

--- INSTALLDOCKER.sh
+++ INSTALLDOCKER.sh
@@ -1,29 +1,41 @@
-#卸载旧版本
+###########################################################
+#This shell is used to install docker on the 64-bit       #
+#version of one of these Ubuntu versions:20.04 18.04 16.04#
+###########################################################
+
+#Uninstall the older versions of Docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
-#更新 apt 包索引
+
+#Update the apt package index and install packages to allow apt to use a repository over HTTPS
 sudo apt-get update
-#安装 apt 依赖包，用于通过HTTPS来获取仓库
+
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
-#添加 Docker 的官方 GPG 密钥
+    
+#Add Docker's offical GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Verify that you now have the key with the fingerprint 
+# 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88, 
+# by searching for the last 8 characters of the fingerprint.
 sudo apt-key fingerprint 0EBFCD88
 
-#使用以下指令设置稳定版仓库-----x86_64 / amd64
+#Use the following command to set up the stable repository.
+#Choose x86_64 / amd64
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-#安装 Docker Engine-Community
+#Update the apt package index, and install the latest version of Docker Engine and containerd
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-#测试安装
+#Verify that Docker Engine is installed correctly by running the hello-world image.
 sudo docker run hello-world
 
 
