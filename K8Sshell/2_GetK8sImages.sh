47ef576ad02e025928a0f556465d00ddfa246e14
fjlinww <34429052+fjlinww@users.noreply.github.com>
2021-01-05 10:10:36 +0800

    Update 2_GetK8sImages.sh

--- K8Sshell/2_GetK8sImages.sh
+++ K8Sshell/2_GetK8sImages.sh
@@ -1,4 +1,7 @@
 # 先执行命令kubeadm config images list，再执行以下脚本
+# 执行脚本如果遇到报错GetK8sImages.sh: 1: GetK8sImages.sh: Syntax error: "(" unexpected
+# 解决办法 sudo dpkg-reconfigure dash  选择no，将默认的shell改为bash
+
 images=(  # 下面的镜像应该去除"k8s.gcr.io/"的前缀，版本换成kubeadm config images list获取到的版本
     kube-apiserver:v1.20.1
     kube-controller-manager:v1.20.1
